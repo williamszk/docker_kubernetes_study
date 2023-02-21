c79ad715d8acda356d75b05dd8479b090d57d6a0
williamszk <wllmszk@gmail.com>
2023-02-21 13:41:17 -0800

    kubernetes tech nana, deployment service mongodb

---
+++ site-kind/230204_01/05_mongodb.sh
@@ -0,0 +1,48 @@
+
+# permission error with docker
+# dial unix /var/run/docker.sock: connect: permission denied
+sudo setfacl --modify user:$USER:rw /var/run/docker.sock
+docker ps -a
+
+minikube status
+minikube start
+
+kubectl get all
+
+# In the host machine ---------------------------------------------------------
+# How to get the name of the host machine?
+path_pem="william-keypair.pem"
+public_dns=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=deleteme-test" | python3 $PWD/aws_cli_filters.py public_dns)
+user="ubuntu"
+user_host="${user}@${public_dns}"
+user_host2="${user_host//[$'\t\r\n']}"
+echo $user_host2
+
+# how to send a local file to a remote machine?
+scp -i $path_pem 230204_01/mongodb-deployment.yaml ${user_host2}:/home/ubuntu
+scp -i $path_pem 230204_01/mongodb-secret.yaml ${user_host2}:/home/ubuntu
+# ----------------------------------------------------------------------------
+
+kubectl apply -f mongodb-secret.yaml
+kubectl apply -f mongodb-deployment.yaml
+
+kubectl delete -f mongodb-deployment.yaml
+kubectl delete -f mongodb-secret.yaml
+
+kubectl get deployment
+kubectl get secret
+
+kubectl get all
+
+kubectl get pod
+
+kubectl describe pod mongodb-deployment-5d966bd9d6-p5b2m 
+
+kubectl get service
+
+kubectl describe service mongodb-service
+
+kubectl describe pod mongodb-deployment-5d966bd9d6-p5b2m
+kubectl get pod -o wide
+
+kubectl get all | grep mongodb
--- site-kind/readme.md
+++ site-kind/readme.md
@@ -3,6 +3,8 @@
 
 https://kind.sigs.k8s.io/docs/user/quick-start/
 
+
+
 # Kubernetes FreeCodeCamp
 
 https://www.youtube.com/watch?v=kTp5xUtcalw&ab_channel=freeCodeCamp.org
@@ -19,8 +21,14 @@ https://learning.edx.org/course/course-v1:LinuxFoundationX+LFS158x+1T2022/block-
 https://learning.edx.org/course/course-v1:LinuxFoundationX+LFS158x+1T2022/block-v1:LinuxFoundationX+LFS158x+1T2022+type@sequential+block@815c1eb1ffb24a7b80bbf9df6d6a3267/block-v1:LinuxFoundationX+LFS158x+1T2022+type@vertical+block@1ce21cfc3f87461cbcd6127e9decfbcd
 
 
+
+
+
 # Youtube TechWorld with Nana
 https://www.youtube.com/watch?v=X48VuDVv0do&ab_channel=TechWorldwithNana
 https://youtu.be/X48VuDVv0do?t=2693
 https://youtu.be/X48VuDVv0do?t=3726
 https://youtu.be/X48VuDVv0do?t=4576
+https://youtu.be/X48VuDVv0do?t=5600
+
+
