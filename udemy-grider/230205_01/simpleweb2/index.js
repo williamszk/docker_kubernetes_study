1bbf5399b3e17144cb26ecf9e312e6f34ac5fde3
williamszk <wllmszk@gmail.com>
2023-03-01 09:17:07 -0300

    docker study grider

--- udemy-grider/230205_01/simpleweb2/index.js
+++ udemy-grider/230205_01/simpleweb2/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
+	// res.send("Hi there again from nodejs");
 	res.send("Hi there");
 });
 
--- udemy-grider/230205_01/simpleweb2/notes.sh
+++ udemy-grider/230205_01/simpleweb2/notes.sh
@@ -1,11 +1,25 @@
 npm i express 
 npm i axios 
 
-cd simpleweb
+cd simpleweb2
 docker build -t williamszk/simpleweb .
 
-docker run -d --name test-container -p 8080:8080 williamszk/simpleweb
+# [inside simpleweb2]
+docker run -d --name test-container \
+    -p 8080:8080 \
+    -v ./index.js:/usr/app/index.js \
+    williamszk/simpleweb
+
+# This will tell that we want to keep the node_module inside the container.
+# -v /usr/app/node_module \
+
+docker ps -a
 docker stop test-container
 docker rm test-container
+docker logs -t test-container
 
 docker exec -it test-container sh 
+
+
+docker stop test-container 
+docker start test-container 
